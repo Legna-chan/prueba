5b484321097c8e32ee04ce6b8292b1076f8220b8
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-08-16 15:05:22 -0700

    Actualizar main-menu.js

--- comandos/main-menu.js
+++ comandos/main-menu.js
@@ -1,21 +1,33 @@
 module.exports = {
+
   name: 'menu',
+
   description: 'Muestra el menú de la bot',
+
   execute(message, args) {
+
     const personaje = {
-      nombre: 'Koniki No Yami',
+
+      nombre: 'Konjiki No Yami',
+
       foto: 'https://files.catbox.moe/aep1ra.jpg'
+
     };
 
     const menu = `
+
 🌸 Hola, bienvenid@ a mi menú! Soy **${personaje.nombre}** 💫
 
 ✐ **Menú de Comandos**
-❀ No hay disponibles por el momento. 
+
+❀ !tiktok ➩ descarga videos de tiktok. 
 
 > Escribe el comando tal como aparece para ejecutarlo.
+
 `;
 
     message.reply({ content: menu, files: [personaje.foto] });
+
   }
+
 };
