4a3e78d837be360695d4594ff2749fa62c4abeb3
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-08-16 13:22:50 -0700

    Actualizar main-menu.js

--- comandos/main-menu.js
+++ comandos/main-menu.js
@@ -1,1 +1,21 @@
+module.exports = {
+  name: 'menu',
+  description: 'Muestra el menú del bot con personaje',
+  execute(message, args) {
+    const personaje = {
+      nombre: 'Nino Nakano',
+      foto: 'https://i.imgur.com/ejemplo.png' // Reemplaza con la URL de la imagen
+    };
 
+    const menu = `
+👋 Hola, bienvenid@ a mi menú! Soy **${personaje.nombre}** 💖
+
+📜 **Menú de Comandos**
+2️⃣ **!hola** - Saluda al usuario
+
+Escribe el comando tal como aparece para ejecutarlo.
+`;
+
+    message.reply({ content: menu, files: [personaje.foto] });
+  }
+};
