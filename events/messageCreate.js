e16909604af316026bd717f7e8136f902e106afb
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-08-16 19:53:42 -0700

    Actualizar messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,10 +3,9 @@ const chalk = require('chalk');
 module.exports = {
   name: 'messageCreate',
   execute(message, client) {
-    // Ignorar mensajes del propio bot
+
     if (message.author.bot) return;
 
-    // Comandos (empiezan con !)
     if (message.content && message.content.startsWith('!')) {
       const args = message.content.slice(1).split(/ +/);
       const commandName = args.shift().toLowerCase();
@@ -16,26 +15,24 @@ module.exports = {
         command.execute(message, args);
       }
     }
-    // Mensaje de texto normal
+
     else if (message.content) {
       console.log(chalk.green(`[MENSAJE] ${message.author.tag}: ${message.content}`));
     }
 
-    // Stickers
     if (message.stickers.size > 0) {
       console.log(chalk.magenta(`[STICKER] ${message.author.tag} envió un sticker`));
     }
 
-    // Archivos adjuntos (fotos, videos)
     if (message.attachments.size > 0) {
       message.attachments.forEach(att => {
         const type = att.contentType || '';
         if (type.includes('image')) {
-          console.log(chalk.yellow(`[IMAGEN] ${message.author.tag} envió una foto`));
+          console.log(chalk.pink(`[IMAGEN] ${message.author.tag} envió una foto`));
         } else if (type.includes('video')) {
           console.log(chalk.cyan(`[VIDEO] ${message.author.tag} envió un video`));
         } else {
-          console.log(chalk.gray(`[ARCHIVO] ${message.author.tag} envió un archivo`));
+          console.log(chalk.red(`[ARCHIVO] ${message.author.tag} envió un archivo`));
         }
       });
     }
