fa2071b912c6870e3213bd711b5bd7dc95c1f103
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-08-17 14:29:31 -0700

    Crear economia-pescar.js

---
+++ comandos/economia-pescar.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const file = "./economy.json";
+
+module.exports = {
+  name: "pescar",
+  description: "Pesca peces y gana monedas",
+  execute(message) {
+    let data;
+    if (!fs.existsSync(file)) {
+      fs.writeFileSync(file, "{}");
+    }
+    data = JSON.parse(fs.readFileSync(file, "utf8"));
+
+    const id = message.author.id;
+    if (!data[id]) data[id] = { balance: 0 };
+
+    const peces = Math.floor(Math.random() * 15) + 1; // 1 a 15 peces
+
+    const monedas = peces * (Math.floor(Math.random() * 5) + 1); // cada pez vale 1-5 monedas
+
+    data[id].balance += monedas;
+
+    fs.writeFileSync(file, JSON.stringify(data, null, 2));
+    message.reply(
+      `🎣 ¡Felicidades! ${message.author.username} pescaste ${peces} peces y ganaste ${monedas} monedas.\n💰 Ahora tienes ${data[id].balance} monedas.`
+    );
+  },
+};
