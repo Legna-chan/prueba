a8d335ace971645601bf129c6082695dddf7a47b
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-08-17 14:39:53 -0700

    Actualizar economia-balance.js

--- comandos/economia-balance.js
+++ comandos/economia-balance.js
@@ -1,19 +1,22 @@
-const { getBalance } = require("../economy.js");
+const fs = require("fs");
+const file = "./economy.json";
+
+function load() {
+  if (!fs.existsSync(file)) fs.writeFileSync(file, "{}");
+  return JSON.parse(fs.readFileSync(file, "utf8"));
+}
 
 module.exports = {
-
   name: "balance",
-
   description: "Muestra tu dinero actual",
-
   execute(message) {
-
+    const data = load();
     const userId = message.author.id;
 
-    const balance = getBalance(userId);
+    if (!data[userId]) data[userId] = { balance: 0, lastDaily: 0 };
+
+    const balance = data[userId].balance;
 
     message.reply(`🍭 Tu saldo actual es: ${balance} monedas.`);
-
-  }
-
+  },
 };
