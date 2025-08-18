c99c92aa3a053230c457f5d045bf47b3fc5ac856
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-08-17 23:35:40 -0700

    Actualizar owner-actualizar.js

--- comandos/owner-actualizar.js
+++ comandos/owner-actualizar.js
@@ -17,13 +17,12 @@ let output = '';
 
 update.stdout.on('data', data => {  
   output += data;  
-  process.stdout.write(data); // Muestra todo en consola  
+  process.stdout.write(data);
 });  
 
 update.stderr.on('data', data => process.stderr.write(data));  
 
 update.on('close', code => {  
-  // Detecta si la bot ya estaba actualizada  
   if (output.includes('Already up to date.') || output.includes('Already up-to-date.')) {  
     message.reply('🌸 La bot ya está actualizada!');  
   } else {  
