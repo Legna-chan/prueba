fa9c9b4844191a7f45963939dde31ae86804247f
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-08-17 22:50:33 -0700

    Actualizar owner-ejecutar.js

--- comandos/owner-ejecutar.js
+++ comandos/owner-ejecutar.js
@@ -1,20 +1,19 @@
 const { exec } = require('child_process');
+const { ownerID } = require('../config.js');
 
 module.exports = {
   name: 'exec',
   description: 'Ejecuta comandos del sistema (solo owner)',
-  execute(message, args, client) {
-    const { ownerID } = require('./config.js');
+  execute(message, args) {
     if (message.author.id !== ownerID) return message.reply('❌ Solo el owner puede usar esto.');
-
-    if (!args[0]) return message.reply('Debes ingresar el comando que deseas ejecutar en el servidor/terminal.');
+    if (!args[0]) return message.reply('Debes ingresar el comando que deseas ejecutar.');
 
     const comando = args.join(' ');
 
     exec(comando, (err, stdout, stderr) => {
-      if (err) console.error(chalk.red(`[ERROR] ${err}`));
-      if (stderr) console.warn(chalk.yellow(`[STDERR] ${stderr}`));
-      if (stdout) console.log(chalk.green(`[STDOUT] ${stdout}`));
+      if (err) console.error(`[ERROR] ${err}`);
+      if (stderr) console.warn(`[STDERR] ${stderr}`);
+      if (stdout) console.log(`[STDOUT] ${stdout}`);
 
       message.reply('🌸 Comando ejecutado. Revisa la consola para detalles.');
     });
