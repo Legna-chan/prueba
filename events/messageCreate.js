ef431c95ebcaf60ef2e8b02332c36f57a247a10d
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-08-26 18:31:43 -0700

    Actualizar messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -20,29 +20,24 @@ module.exports = {
       console.log(chalk.green(`[MENSAJE] ${message.author.tag}: ${message.content}`));
     }
 
+    if (message.content) {
+      const gifLink = message.content.match(/https?:\/\/\S+\.gif/i);
+      if (gifLink) {
+        console.log(chalk.cyan(`[GIF-LINK] ${message.author.tag} envió un GIF: ${gifLink[0]}`));
+      }
+    }
+
     if (message.stickers.size > 0) {
-      console.log(chalk.magenta(`[STICKER] ${message.author.tag} envió un sticker`));
+      message.stickers.forEach(sticker => {
+        if (sticker.format === 2) { // Sticker animado (GIF)
+          console.log(chalk.cyan(`[GIF-STICKER] ${message.author.tag} envió un sticker animado`));
+        } else {
+          console.log(chalk.magenta(`[STICKER] ${message.author.tag} envió un sticker`));
+        }
+      });
     }
 
     if (message.attachments.size > 0) {
       message.attachments.forEach(att => {
         const type = att.contentType || '';
-        const name = att.name || '';
-
-        if (type.includes('image') || name.match(/\.(png|jpg|jpeg|gif)$/i)) {
-          if (type.includes('gif') || name.endsWith('.gif')) {
-            console.log(chalk.cyan(`[GIF] ${message.author.tag} envió un GIF`));
-          } else {
-            console.log(chalk.magenta(`[IMAGEN] ${message.author.tag} envió una foto`));
-          }
-        } else if (type.includes('video') || name.match(/\.(mp4|mov|avi|mkv)$/i)) {
-          console.log(chalk.cyan(`[VIDEO] ${message.author.tag} envió un video`));
-        } else if (type.includes('audio') || name.match(/\.(mp3|wav|ogg|m4a)$/i)) {
-          console.log(chalk.yellow(`[AUDIO] ${message.author.tag} envió un audio`));
-        } else {
-          console.log(chalk.white(`[ARCHIVO] ${message.author.tag} envió un archivo`));
-        }
-      });
-    }
-  }
-};
+        const name
