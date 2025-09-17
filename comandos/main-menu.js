aa0983de83014b5d05260c57cad82febe1865e19
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-09-16 22:51:34 -0700

    Actualizar main-menu.js

--- comandos/main-menu.js
+++ comandos/main-menu.js
@@ -6,26 +6,26 @@ module.exports = {
   execute(message, args) {
     const menuEmbed = new EmbedBuilder()
       .setColor('#FF69B4')
-      .setTitle('🌈 ｡ﾟﾟ･ Menú oficial de la bot ･ﾟﾟ｡ 🌈')
+      .setTitle('🍃 ｡ﾟﾟ･ Menú oficial de la bot ･ﾟﾟ｡ 🍃')
       .setDescription(`
 🌸 ¡Bienvenid@ a **Konjiki No Yami**! 🌸
 
 🎆 **General**
-🎐 !menu → Muestra este menú.
+🎐 #menu → Muestra este menú.
 
 🎆 **Descargas**
-🎐 !tiktok → Descarga videos de tiktok.
+🎐 #tiktok → Descarga videos de tiktok.
 
 🎆 **Economía**
-🎐 !daily → Recompensa diaria.
-🎐 !balance → Ver tu dinero actual.
-🎐 !pescar → Gana monedas pescando.
+🎐 #daily → Recompensa diaria.
+🎐 #balance → Ver tu dinero actual.
+🎐 #pescar → Gana monedas pescando.
 
 🎆 **Reacciones Anime**
-🎐 !happy → Expresa felicidad con un gif de anime.
+🎐 #happy → Expresa felicidad con un gif de anime.
 
 🎆 **Entretenimiento**
-🎐 !meme → Manda un meme aleatorio.
+🎐 #meme → Manda un meme aleatorio.
       `)
       .setImage('https://files.catbox.moe/aep1ra.jpg')
       .setFooter({ text: 'Editada y desarrollada por @LegnaAm' });
