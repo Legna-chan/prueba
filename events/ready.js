2f56e49144b34c7ad21a96a9c0a58c9ad2dad9d2
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-10-22 18:47:55 -0700

    Delete events directory

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-const chalk = require('chalk');
-
-module.exports = {
-  name: 'ready',
-  once: true,
-  execute(client) {
-    console.log(chalk.yellow(`🍭 Bot conectada con exito como ${client.user.tag}`));
-  }
-};
